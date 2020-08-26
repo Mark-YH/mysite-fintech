98c6a89f1fa80faaeb47c444aa96d2e786209294
Mark <s108321505@mail1.ncnu.edu.tw>
2020-08-26 18:23:16 +0800

    API test

--- fintech/views.py
+++ fintech/views.py
@@ -1,3 +1,76 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+def test(request):
+    if request.method == 'GET':
+        context = {
+            "version1": {
+                "training": {
+                    "Y2Y": {
+
+                    },
+                    "Y2H": {
+
+                    },
+                    "Y2Q": {
+
+                    }
+                },
+                "testing": {
+                    "Y2Y": {
+
+                    },
+                    "Y2H": {
+
+                    },
+                    "Y2Q": {
+
+                    }
+                },
+                "params": {
+                    "population": 10,
+                    "generation": 10000,
+                    "theta": 0.004,
+                    "init fund": 1e7,
+                    "encoding bit": 7
+                }
+            },
+            "version2": {
+                "training": {
+                    "Y2Y": {
+                        "periods": [
+
+                        ],
+                        "execution time": 123
+                    },
+                    "Y2H": {
+
+                    },
+                    "Y2Q": {
+
+                    }
+                },
+                "testing": {
+                    "Y2Y": {
+
+                    },
+                    "Y2H": {
+
+                    },
+                    "Y2Q": {
+
+                    }
+                },
+                "params": {
+                    "population": 10,
+                    "generation": 10000,
+                    "theta": 0.004,
+                    "init fund": 1e7,
+                    "encoding bit": 7
+                }
+            }
+        }
+        return JsonResponse(context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from fintech.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/test/', test),
 ]
